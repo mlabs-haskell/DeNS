274765af3a4380f73407662bb7667d68153d901b
jared <>
2024-01-07 01:11:44 -0700

    Added `flake.nix`, `.gitignore`, hercules CI, and pre-commit checks

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DeNS
+
 Decentralized Name System
---
+++ pre-commit.nix
@@ -0,0 +1,14 @@
+{ inputs, ... }: {
+  imports = [ inputs.pre-commit-hooks-nix.flakeModule ];
+  perSystem = { config, ... }: {
+    devShells.dev-pre-commit = config.pre-commit.devShell;
+    pre-commit.settings = {
+      hooks = {
+        nixpkgs-fmt.enable = true;
+        deadnix.enable = true;
+        typos.enable = true;
+        markdownlint.enable = true;
+      };
+    };
+  };
+}
