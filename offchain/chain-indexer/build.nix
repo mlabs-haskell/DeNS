9725a748929efe1d580670b5ba19cfc98c109c91
jared <>
2024-01-23 16:09:09 -0700

    Created empty draft of `chain-indexer`

--- offchain/chain-indexer/build.nix
+++ offchain/chain-indexer/build.nix
@@ -1,2 +1,28 @@
-_:
-{ }
+{ inputs, ... }:
+{
+  config = {
+    perSystem = { system, config, ... }:
+      let
+        tsFlake = inputs.flake-lang.lib.${system}.typescriptFlake {
+          name = "chain-indexer";
+          src = ./.;
+          inherit (config.settings)
+            devShellTools
+            devShellHook;
+        };
+      in
+      {
+        packages = {
+          inherit (tsFlake.packages)
+            chain-indexer-typescript
+            chain-indexer-typescript-tgz;
+        };
+
+        devShells = {
+          inherit (tsFlake.devShells) chain-indexer-typescript;
+        };
+
+        inherit (tsFlake) checks;
+      };
+  };
+}
