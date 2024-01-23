84162eeb0b56c616a659a1acc6bf14feb392295e
jared <>
2024-01-23 16:07:53 -0700

    Created empty lucid project for `tx-builder`

--- offchain/tx-builder/build.nix
+++ offchain/tx-builder/build.nix
@@ -1,2 +1,28 @@
-_:
-{ }
+{ inputs, ... }:
+{
+  config = {
+    perSystem = { system, config, ... }:
+      let
+        tsFlake = inputs.flake-lang.lib.${system}.typescriptFlake {
+          name = "tx-builder";
+          src = ./.;
+          inherit (config.settings)
+            devShellTools
+            devShellHook;
+        };
+      in
+      {
+        packages = {
+          inherit (tsFlake.packages)
+            tx-builder-typescript
+            tx-builder-typescript-tgz;
+        };
+
+        devShells = {
+          inherit (tsFlake.devShells) tx-builder-typescript;
+        };
+
+        inherit (tsFlake) checks;
+      };
+  };
+}
