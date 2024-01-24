a5253a645877941d497bba3850b20dea895505c5
jared <>
2024-01-23 17:45:27 -0700

    Added some documentation

--- offchain/chain-indexer/build.nix
+++ offchain/chain-indexer/build.nix
@@ -18,6 +18,12 @@
             chain-indexer-typescript-tgz;
         };
 
+        # When developing, in this directory, run
+        # ```bash
+        # nix develop .#chain-indexer-typescript
+        # ```
+        # and it'll give you some goodies (`node_modules/` for dependencies +
+        # `./extra-dependencies`).
         devShells = {
           inherit (tsFlake.devShells) chain-indexer-typescript;
         };
--- offchain/tx-builder/build.nix
+++ offchain/tx-builder/build.nix
@@ -18,6 +18,12 @@
             tx-builder-typescript-tgz;
         };
 
+        # When developing, in this directory, run
+        # ```bash
+        # nix develop .#tx-builder-typescript
+        # ```
+        # and it'll give you some goodies (`node_modules/` for dependencies +
+        # `./extra-dependencies`).
         devShells = {
           inherit (tsFlake.devShells) tx-builder-typescript;
         };
