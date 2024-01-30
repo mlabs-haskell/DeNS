a75bf04c6142e224eb009ddd2e2056c8ce3f2f9e
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-01-30 01:31:53 -0600

    temporal save

---
+++ dens-query/src/DensQuery/ChainIndexer.ts
@@ -0,0 +1,9 @@
+import { main } from "./Arweave/Arweave.js";
+
+//import Wallet from 'ethereumjs-wallet';
+//
+//let generated = Wallet.default.generate()
+//
+//console.log("address: "+generated.getAddressString())
+//console.log("private: "+generated.getPrivateKeyString())
+main();
