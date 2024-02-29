aebfc4ea018a17afc9b8466fb3dad3a6d2e200f5
jared <>
2024-02-29 07:29:21 -0700

    Code cleanup + switch to cardano-serialization-lib

--- dens-query/src/DensQuery/Config.ts
+++ dens-query/src/DensQuery/Config.ts
@@ -19,6 +19,7 @@ import { readFile } from "node:fs/promises";
 export { Config, DbConfig, OgmiosConfig };
 
 const DENS_QUERY_CONFIG_ENV_VAR = "DENS_QUERY_CONFIG";
+const DENS_QUERY_INIT_SQL_FILE_ENV_VAR = "DENS_QUERY_INIT_SQL_FILE";
 
 /**
  * {@link initConfig} reads the environment variable {@link
@@ -28,7 +29,7 @@ export async function initConfig(): Promise<Config> {
   const configFile = process.env[DENS_QUERY_CONFIG_ENV_VAR];
   if (configFile === undefined) {
     throw new Error(
-      `error: environment variable ${DENS_QUERY_CONFIG_ENV_VAR} is undefined. Please provide a configuration file.`,
+      `environment variable ${DENS_QUERY_CONFIG_ENV_VAR} is undefined. Please provide a configuration file.`,
     );
   }
   const contents = await readFile(configFile, { encoding: "utf8" });
@@ -39,4 +40,12 @@ export async function initConfig(): Promise<Config> {
 }
 
 export const config: Config = await initConfig();
-export default config;
+export const initSqlFile: string = (() => {
+  const initSqlFile = process.env[DENS_QUERY_INIT_SQL_FILE_ENV_VAR];
+  if (initSqlFile === undefined) {
+    throw new Error(
+      `environment variable ${DENS_QUERY_INIT_SQL_FILE_ENV_VAR} is undefined, so no init SQL file was found.`,
+    );
+  }
+  return initSqlFile;
+})();
